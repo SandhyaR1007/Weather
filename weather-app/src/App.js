5ad18e4b74e56262f6a5788cd16e8d24ee396ead
SandhyaR1007 <grajwanshi1008@gmail.com>
2021-05-18 00:31:59 +0530

    updated app.js

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -3,28 +3,102 @@ import Card from "./Card";
 import "./weather1.jpg";
 import { useState, useEffect } from "react";
 
+
 function App() {
   const [display,setDisplay] = useState({
     "desc":"Haze",
     "temp":38,
-    "icon":0 
+    "icon":800,
+    "city":"Delhi" 
 
   })
-  // try {
-  //   useEffect(() => {
-  //     fetch(
-  //       "https://api.weatherbit.io/v2.0/current?city=delhi&country=india&key=140c2a81818b4c839f4c030c37e1d794"
-  //     )
-  //       .then((res) => res.json())
-  //       .then((json) => setDisplay({"desc":json.data[0].weather.description,"temp":json.data[0].temp,"icon":json.data[0].weather.icon}));
-  //   });
-  // } catch (error) {
-  //   console.log("not working");
-  // }
+  const src = "clear.png"
+  const [city,setCity] = useState("")
+  const [result,setResult] = useState("delhi")
+  const [icon,SetIcon] = useState("fas fa-sun")
+  
+
+  const cityFinder = (e)=>{
+    setCity(e.target.value)
+
+  }
+  const iconHandler = ()=>{
+    //thunderstorm
+    if(display.icon>=200 && display.icon<=233){
+      SetIcon("fas fa-bolt")
+    }
+    // drizzle
+    else if(display.icon>=300 && display.icon<=302){
+      SetIcon("fas fa-cloud-rain")
+    }
+    //rainfall
+    else if((display.icon>=500 && display.icon<=522)||(display.icon == 900)){
+      SetIcon("fas fa-cloud-showers-heavy")
+    }
+    //snow
+    else if((display.icon>=600 && display.icon<=610)||(display.icon>=621 && display.icon<=623)){
+      SetIcon("fas fa-snowflake")
+    }
+    //sleet
+    else if(display.icon== 611 || display.icon==612){
+      SetIcon("fas fa-wind")
+    }
+    //haze
+    else if(display.icon>=700 && display.icon<=751){
+      SetIcon("fas fa-smog")
+    }
+  
+    //few clouds
+    else if(display.icon>=801 && display.icon<=803){
+      SetIcon("fas fa-cloud-sun")
+    }
+    //overcast
+    else if(display.icon==804){
+      SetIcon("fas fa-cloud")
+    }
+    else{
+      SetIcon("fas fa-sun")
+    }
+    
+}
+
+  const resultSetter = (e)=>{
+    e.preventDefault();
+    setResult(city)
+    console.log("working")
+    iconHandler()
+    console.log("fine")
+
+    
+    
+   
+  }
+  
+
+  
+  //   try {
+  //     useEffect(() => {
+  //       fetch(
+  //        `https://api.weatherbit.io/v2.0/current?city=${result}&country=india&key=140c2a81818b4c839f4c030c37e1d794`
+  //       )
+  //         .then((res) => res.json())
+  //         .then((json) => setDisplay({"desc":json.data[0].weather.description,
+  //         "temp":json.data[0].temp,
+  //         "icon":json.data[0].weather.code,
+  //        "city":json.data[0].city_name}));
+  //     });
+  //   } catch (error) {
+  //    console.log("not working");
+  //  }
   return (
-    <div className="App">
+    <div className="App" >
+            <form className="search-area" onSubmit={resultSetter}>
+                <input className="search-place" placeholder="enter a city" onChange={cityFinder}/>
+                <button className="search-btn" type="submit">Search</button>
+    </form>
+    
       
-      <Card temperature={display.temp} weather={display.desc} place={"New Delhi"} icon={display.icon}/>
+      <Card temperature={display.temp} weather={display.desc} place={display.city} icon={icon}/>
       
       
     </div>
--- weather-app/src/Card.js
+++ weather-app/src/Card.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import "./Card.css"
 
+
+
+
+
 function Card(props) {
     const week = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
     const year = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
@@ -8,9 +12,15 @@ function Card(props) {
     const day = d.getDay();
     const  date = d.getDate();
     const month  = d.getMonth();
+
+   
+
+
     return (
+
         
         <div className="card">
+            
             <div className="header">
                 <div className="place">{props.place}</div>
                 <div className="day-date">
@@ -24,10 +34,13 @@ function Card(props) {
                 <div className="temp">{props.temperature}<span className="deg">oC</span></div>
             </div>
             <div className="footer sub-card">
-                <div className="icon">{props.icon}</div>
+               
+            <i class={props.icon}></i>
+           
+            
                 <div className="weather">{props.weather}</div>
             </div>
-            <button className="place-btn">Change place</button>
+            
              
             
     
